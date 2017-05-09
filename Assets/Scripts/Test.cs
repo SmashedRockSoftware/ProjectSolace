dde51d1b42eb2f8f8cf0fd6c4a351e4fb8835e64
bgray <bgray8898@gmail.com>
2017-05-08 23:57:17 -0700

    Added Test Script
    A completely useless script.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Are you happy?
+
+    private void Start(){
+        Debug.Log("HERE YOU ARE");
+    }
+}

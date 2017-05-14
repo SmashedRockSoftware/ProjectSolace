7eade577ed30d1150aae7a19fb1c33de94c3f514
bgray <bgray8898@gmail.com>
2017-05-14 16:05:13 -0700

    Dialogue Choices
    Some work on the choice system in the dialogue UI manager.

--- Assets/Scripts/Dialogue_System/DialogueUIManager.cs
+++ Assets/Scripts/Dialogue_System/DialogueUIManager.cs
@@ -6,7 +6,10 @@ public class DialogueUIManager : MonoBehaviour, IPointerClickHandler {
 
 	// Test dialog
 	private readonly string[] names = new string[]{"Murderer", "Player", "DONALD TRUMP"};
-	private readonly string[] dialogue = new string[]{"I will kill Alex Jones.", "Sounds good bro.", "Well, that's all the text we've got. Click to repeat."};
+	private readonly string[] dialogue = new string[]{"I will kill Alex Jones.", "&choice", "Well, that's all the text we've got. Click to repeat."};
+	private readonly string[] choices = new string[]{"Execute Order 66", "Gas 'em", "Make sure it's painless.", "Make sure it's painful."};
+
+	private const string ChoiceString = "&choice"; // Constant for marking a choice in dialogue
 
 	private string currentDialog; // Current dialog of the system
 	private int currentChar = 0; // Current char of the dialog
@@ -15,6 +18,9 @@ public class DialogueUIManager : MonoBehaviour, IPointerClickHandler {
 
 	private Text nameText; // Text component of name text
 	private Text dialogueText; // Text component of dialog text
+
+	public Text[] choiceText; // Text component of choice buttons, assigned in editor
+	public Button[] choiceButtons; // Button component of choice buttons, assigned in editor
 
 	private int currentFrame = -1;
 	private float dialogSpeed = 0.03f; // In seconds per character
@@ -64,13 +70,12 @@ public class DialogueUIManager : MonoBehaviour, IPointerClickHandler {
 		if (!addingChars || eventData.button == PointerEventData.InputButton.Right) {
 			if (eventData.button == PointerEventData.InputButton.Left) {
 				currentFrame++; // Progress dialog on left click
-			}
-			else if (eventData.button == PointerEventData.InputButton.Right) {
+			}else if (eventData.button == PointerEventData.InputButton.Right) {
 				currentFrame--; // Regress dialog on right click
 			}
 
 
-			// Resetting dialog
+			// Resetting dialog, should be removed/made general later
 			if (currentFrame > 2) {
 				currentFrame = 0;
 			}
@@ -78,16 +83,34 @@ public class DialogueUIManager : MonoBehaviour, IPointerClickHandler {
 				currentFrame = 0;
 			}
 
-			// Set new name text and buffer new dialog text
-			nameText.text = names[currentFrame];
-			currentDialog = dialogue[currentFrame];
+			// Check if next dialogue node is a choice or not
+			if (dialogue[currentFrame].Equals(ChoiceString)) { // If a choice
 
-			// Reset dialog box, lastTime, and currentChar
-			dialogueText.text = "";
-			lastTime = 0;
-			currentChar = 0;
+				SetChoiceButtonEnable(true, choices.Length); // Enable buttons
+				dialogueText.text = ""; // Remove the previous text from the screen
 
-			addingChars = true; // We're adding chars now
+				// Fill in choice text with choices and enable
+				for (int i = 0; i < choices.Length; i++) {
+					choiceText[i].text = choices[i];
+				}
+
+			} else { // Not a choice
+
+				// Disable all choice buttons
+				SetChoiceButtonEnable(false, 4);
+
+				// Set new name text and buffer new dialog text
+				nameText.text = names[currentFrame];
+				currentDialog = dialogue[currentFrame];
+
+				// Reset dialog box, lastTime, and currentChar
+				dialogueText.text = "";
+				lastTime = 0;
+				currentChar = 0;
+
+				addingChars = true; // We're adding chars now
+
+			}
 
 		} else { // Finishing current text if we're in some
 
@@ -96,6 +119,17 @@ public class DialogueUIManager : MonoBehaviour, IPointerClickHandler {
 
 		}
 
+	}
+
+	/// <summary>
+	/// 	Sets the enable state of choice buttons from 0 to max - 1.
+	/// </summary>
+	/// <param name="enabled">Whether to enable or disable buttons.</param>
+	/// <param name="max">The maximum button to change.</param>
+	private void SetChoiceButtonEnable(bool enabled, int max) {
+		for (int i = 0; i < max; i++) {
+			choiceButtons[i].gameObject.SetActive(enabled);
+		}
 	}
 
 	/// <summary>
